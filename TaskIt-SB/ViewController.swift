ed0ddb509ac848a76ec4a738e99696f65a527a0e
dalogrus <sburek+github@gmail.com>
2015-04-19 16:11:01 +0200

    Initial Commit

---
+++ TaskIt-SB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaskIt-SB
+//
+//  Created by Sebastian Burek on 19.04.2015.
+//  Copyright (c) 2015 Sebastian Burek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
