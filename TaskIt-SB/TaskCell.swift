6d5b0c36038243813313bbb613fefb1e34d3dc7b
dalogrus <sburek+github@gmail.com>
2015-04-25 23:05:08 +0200

    second commit

---
+++ TaskIt-SB/TaskCell.swift
@@ -0,0 +1,29 @@
+//
+//  TaskCell.swift
+//  TaskIt-SB
+//
+//  Created by Sebastian Burek on 23.04.2015.
+//  Copyright (c) 2015 Sebastian Burek. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    @IBOutlet weak var taskLabel: UILabel!    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
