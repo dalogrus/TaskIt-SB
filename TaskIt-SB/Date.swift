c85ba32d13a62a51d42a665ebee27fc1bf41974f
dalogrus <sburek+github@gmail.com>
2015-04-26 21:53:09 +0200

    fourth commit

---
+++ TaskIt-SB/Date.swift
@@ -0,0 +1,33 @@
+//
+//  Date.swift
+//  TaskIt-SB
+//
+//  Created by Sebastian Burek on 26.04.2015.
+//  Copyright (c) 2015 Sebastian Burek. All rights reserved.
+//
+
+import Foundation
+
+class Date {
+    class func from (#year:Int, month:Int, day:Int) -> NSDate {
+        
+        var components = NSDateComponents()
+        components.year = year
+        components.month = month
+        components.day = day
+        
+        var gregorianCalendar = NSCalendar(identifier: NSGregorianCalendar)
+        var date = gregorianCalendar?.dateFromComponents(components)
+        
+        return date!
+    }
+    
+    class func toString(#date:NSDate) -> String {
+        
+        let dateStringFormatter = NSDateFormatter()
+        dateStringFormatter.dateFormat = "yyyy-MM-dd"
+        let dateString = dateStringFormatter.stringFromDate(date)
+        
+        return dateString
+    }
+}
--- TaskIt-SB/ViewController.swift
+++ TaskIt-SB/ViewController.swift
@@ -18,12 +18,16 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let task1 = TaskModel(task: "Study French", subTask: "Verbs", date: "01/14/2014")
-        let task2 = TaskModel(task: "Eat Dinner", subTask: "Buregers", date: "01/14/2014")
+        let date1 = Date.from(year: 2014, month: 05, day: 20)
+        let date2 = Date.from(year: 2014, month: 03, day: 3)
+        let date3 = Date.from(year: 2014, month: 12, day: 13)
+        
+        let task1 = TaskModel(task: "Study French", subTask: "Verbs", date: date1)
+        let task2 = TaskModel(task: "Eat Dinner", subTask: "Burgers", date: date2)
         //task3 can also be defined directly in the taskArray array
         //let task3 = TaskModel(task: "Gym", subTask: "Leg Day", date: "01/14/2014")
         
-        taskArray = [task1, task2, TaskModel(task: "Gym", subTask: "Leg Day", date: "01/14/2014")]
+        taskArray = [task1, task2, TaskModel(task: "Gym", subTask: "Leg Day", date: date3)]
         
         self.tableView.reloadData()
     }
@@ -31,6 +35,16 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if segue.identifier == "showTaskDetail" {
+            let detailVC: TaskDetailViewController = segue.destinationViewController as! TaskDetailViewController
+            let indexPath = self.tableView.indexPathForSelectedRow()
+            let thisTask = taskArray[indexPath!.row]
+            detailVC.detailTaskModel = thisTask
+        }
     }
     
     //UITableViewDataSource
@@ -46,7 +60,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         cell.taskLabel.text = thisTask.task
         cell.descriptionLabel.text = thisTask.subTask
-        cell.dateLabel.text = thisTask.date
+        cell.dateLabel.text = Date.toString(date: thisTask.date)
         
         return cell
         }
