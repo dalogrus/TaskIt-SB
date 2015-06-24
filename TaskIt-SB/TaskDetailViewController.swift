ccd2aa6dc5816cfcc7c8d05c7716cdaa4a1f306d
dalogrus <sburek@gmail.com>
2015-06-25 00:12:14 +0200

    implementation of CoreData

--- TaskIt-SB/TaskDetailViewController.swift
+++ TaskIt-SB/TaskDetailViewController.swift
@@ -12,8 +12,6 @@ class TaskDetailViewController: UIViewController {
 
     var detailTaskModel: TaskModel!
     
-    var mainVC: ViewController!
-    
     @IBOutlet weak var taskTextField: UITextField!
     @IBOutlet weak var subtaskTextField: UITextField!
     @IBOutlet weak var dueDatePicker: UIDatePicker!
@@ -24,7 +22,7 @@ class TaskDetailViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         self.taskTextField.text = detailTaskModel.task
-        self.subtaskTextField.text = detailTaskModel.subTask
+        self.subtaskTextField.text = detailTaskModel.subtask
         self.dueDatePicker.date = detailTaskModel.date
     }
 
@@ -37,9 +35,14 @@ class TaskDetailViewController: UIViewController {
     }
     @IBAction func doneBarButtonItemPressed(sender: UIBarButtonItem) {
         
-        var task = TaskModel(task: taskTextField.text, subTask: subtaskTextField.text, date: dueDatePicker.date, completed: false)
+        let appDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
         
-        mainVC.baseArray[0][mainVC.tableView.indexPathForSelectedRow()!.row] = task
+        detailTaskModel.task = taskTextField.text
+        detailTaskModel.subtask = subtaskTextField.text
+        detailTaskModel.date = dueDatePicker.date
+        detailTaskModel.completed = detailTaskModel.completed
+        
+        appDelegate.saveContext()
         
         self.navigationController?.popViewControllerAnimated(true)
     }
